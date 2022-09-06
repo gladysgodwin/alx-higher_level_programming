fa0fb27af2cc009c2de7a66cca29e747fe9d6e49
Gladys Oviawe <gladysgodwin79@gmail.com>
2022-09-06 18:53:34 +0000

    multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-let c = "C is fun";
-let p = "Python is cool";
-let j = "JavaScript is amazing";
-
-console.log(c\n);
-console.log(p\n);
-console.log(j);
+const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
+console.log(myVar[0]);
+console.log(myVar[1]);
+console.log(myVar[2]);

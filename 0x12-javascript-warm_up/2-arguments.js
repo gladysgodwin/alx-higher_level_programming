3b41df4571780e28bae073f488829a64fc2917fc
Gladys Oviawe <gladysgodwin79@gmail.com>
2022-09-06 19:54:51 +0000

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = ["python", "javascript"]; 
+  if (myVar < 1){
+    console.log("No argument");
+  } else if (myVar > 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+}
